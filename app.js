e19a8849858fc2ec93ffddf20b1243edf8822043
KathiaAmador <157557206+KathiaAmador@users.noreply.github.com>
2024-01-24 14:33:17 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
-console.log("Ejecutando el sistema")
+Const letrasEncriptadas = {
+  "e": "enter",
+  "i": "imes",
+  "a": "ai",
+  "o": "ober",
+  "u": "ufat"
+};
+
+function encriptar(texto) {
+  const textoEncriptado ="";
+
+for (let i = 0; i < texto.length; i++) {
+  let letra = texto.charAt(i);
+
+  if (letrasEncriptadas.hasOwnProperty(letras)) {
+    letraEncriptado += letrasEncriptadas[letra];
+  } else {
+    letraEncriptado += letra;
+  }
+}
+
+return textoEncriptado;
+}
+
+function desencriptador(textoEncriptado) {
+  const textoDesencriptado = "";
+  for (let i = 0; i < textoEncriptado.length; i++) {
+    let letra = textoEncriptado.charAt(i);
+
+    if (letrasEncriptadas.hasOwnProperty(letra)) {
+      letraDesencriptado += letrasEn
